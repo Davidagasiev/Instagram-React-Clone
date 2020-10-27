525faf077647636a964823e3a047f8fc2c31e4dc
David Agasiev <davidagasiev2005@gmail.com>
2020-10-28 00:51:04 +0400

    Removing Instagram Embed

--- src/Main.js
+++ src/Main.js
@@ -2,7 +2,7 @@ import React from "react";
 import PostList from "./PostGrid/PostList";
 import mergeSort from "./Hooks/mergeSort"
 
-import InstagramEmbed from 'react-instagram-embed';
+import "./Main.css";
 
 
 function Main(props) {
@@ -17,14 +17,40 @@ function Main(props) {
 {/**************************** Instagram Sidebar 8*************************************/}
             <div className="embed">
               <div className="embed2">
-                <InstagramEmbed
-                  url='https://www.instagram.com/p/CEKeX08MKxy/'
-                  maxWidth={400}
-                  hideCaption={false}
-                  containerTagName='div'
-                  protocol=''
-                  injectScript
-                />
+                <p className="find">Find me on:</p>
+              
+                <ul className="links">
+                  <li>
+                    <a href="https://www.linkedin.com/in/david-agasiev/" target="_blank">
+                      <img src="https://cdn4.iconfinder.com/data/icons/social-messaging-ui-color-shapes-2-free/128/social-linkedin-circle-512.png"/>
+                    </a>
+                  </li>
+
+                  <li >
+                    <a href="https://github.com/Davidagasiev" target="_blank">
+                      <img src="https://cdn0.iconfinder.com/data/icons/octicons/1024/mark-github-512.png"/>
+                    </a>
+                  </li>
+
+                  <li>
+                    <a href="https://www.instagram.com/david_agas/" target="_blank">
+                      <img src="https://cdn4.iconfinder.com/data/icons/social-messaging-ui-color-shapes-2-free/128/social-instagram-new-circle-512.png"/>
+                    </a>
+                  </li>
+
+                  <li>
+                    <a href="https://www.facebook.com/devi.agasiev" target="_blank">
+                      <img src="https://cdn0.iconfinder.com/data/icons/social-messaging-ui-color-shapes-2-free/128/social-facebook-2019-circle-512.png"/>
+                    </a>
+                  </li>
+                </ul>
+
+                <p className="messengerP">Also check my Messenger Clone</p>
+                <div className="messengerClone">
+                  <a href="https://facebook-messenger-react-clone.web.app/" target="_blank">
+                    <img src="https://facebookbrand.com/wp-content/uploads/2020/10/Logo_Messenger_NewBlurple-399x399-1.png?w=399&h=399"/>
+                  </a>
+                </div>
               </div>
             </div>
 {/**************************** Instagram Sidebar 8*************************************/}
